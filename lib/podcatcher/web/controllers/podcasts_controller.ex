318387cad1f484f726081800d993a4c07f3f049b
Dan Jacob <danjac354@gmail.com>
2017-04-11 20:30:05 +0300

    List redesign

--- lib/podcatcher/web/controllers/podcasts_controller.ex
+++ lib/podcatcher/web/controllers/podcasts_controller.ex
@@ -4,15 +4,14 @@ defmodule Podcatcher.Web.PodcastsController do
   alias Podcatcher.Podcasts
   alias Podcatcher.Episodes
 
+  def index(conn, %{"q" => ""} = params), do: latest_podcasts(conn, params)
+
   def index(conn, %{"q" => search_term} = params) do
     page = Podcasts.search_podcasts(search_term, params)
     render conn, "index.html", %{page: page, search_term: search_term, page_title: "Discover"}
   end
 
-  def index(conn, params) do
-    page = Podcasts.latest_podcasts(params)
-    render conn, "index.html", %{page: page, search_term: nil, page_title: "Discover"}
-  end
+  def index(conn, params), do: latest_podcasts(conn, params)
 
   def podcast(conn, %{"id" => id, "q" => search_term} = params) do
     podcast = Podcasts.get_podcast!(id)
@@ -30,4 +29,10 @@ defmodule Podcatcher.Web.PodcastsController do
     page = Episodes.episodes_for_podcast(podcast, params)
     render conn, "podcast.html", %{podcast: podcast, page: page, page_title: podcast.title}
   end
+
+  defp latest_podcasts(conn, params) do
+    page = Podcasts.latest_podcasts(params)
+    render conn, "index.html", %{page: page, search_term: nil, page_title: "Discover"}
+  end
+
 end
