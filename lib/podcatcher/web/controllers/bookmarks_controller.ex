43e2c699d5b897e9fd593bd2efb9c026903649cd
Dan Jacob <danjac354@gmail.com>
2017-04-16 15:01:11 +0300

    Add search to bookmarks

--- lib/podcatcher/web/controllers/bookmarks_controller.ex
+++ lib/podcatcher/web/controllers/bookmarks_controller.ex
@@ -6,10 +6,13 @@ defmodule Podcatcher.Web.BookmarksController do
 
   plug Podcatcher.Web.Plugs.RequireAuth
 
-  def index(conn, _params) do
-    page = Bookmarks.bookmarks_for_user(conn.assigns[:user])
-    render conn, "index.html", page: page
+  def index(conn, %{"q" => search_term} = params) do
+    page = Bookmarks.search_bookmarks_for_user(conn.assigns[:user], search_term, params)
+    render conn, "index.html", page: page, search_term: search_term
   end
+
+  def index(conn, %{"q" => ""} = params), do: list_bookmarks(conn, params)
+  def index(conn, params), do: list_bookmarks(conn, params)
 
   def add_bookmark(conn, %{"id" => episode_id}) do
     episode = Episodes.get_episode!(episode_id)
@@ -21,6 +24,11 @@ defmodule Podcatcher.Web.BookmarksController do
     episode = Episodes.get_episode!(episode_id)
     Bookmarks.delete_bookmark(conn.assigns[:user], episode)
     send_resp(conn, :ok, "Bookmark deleted")
+  end
+
+  defp list_bookmarks(conn, params) do
+    page = Bookmarks.bookmarks_for_user(conn.assigns[:user], params)
+    render conn, "index.html", page: page, search_term: nil
   end
 
 end
