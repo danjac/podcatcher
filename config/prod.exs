066a9e7bb33e4b94f736fb37ad75726a984e5a08
Dan Jacob <danjac@localhost.localdomain>
2017-05-03 15:27:20 +0300

    https config

--- config/prod.exs
+++ config/prod.exs
@@ -15,9 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :podcatcher, Podcatcher.Web.Endpoint,
   on_init: {Podcatcher.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "beta.podbaby.me", port: 443],
   https: [port: {:system, "PORT"}],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "beta.podbaby.me", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
