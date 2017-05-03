68db243906fa1ec8f194758dc934d103ac6f93de
Dan Jacob <danjac@localhost.localdomain>
2017-05-03 14:36:12 +0300

    URL config for production

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :podcatcher, Podcatcher.Web.Endpoint,
   on_init: {Podcatcher.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
+  url: [host: "beta.podbaby.me", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
