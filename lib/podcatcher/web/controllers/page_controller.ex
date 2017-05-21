257897086b66698106b1278eff51ed0a9c115148
Dan Jacob <danjac@localhost.localdomain>
2017-05-21 23:37:38 +0300

    Front page redirects to feed if user logged in

--- lib/podcatcher/web/controllers/page_controller.ex
+++ lib/podcatcher/web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule Podcatcher.Web.PageController do
   use Podcatcher.Web, :controller
 
+  def index(%Plug.Conn{assigns: %{user: _}} = conn, _params) do
+    redirect conn, to: subscriptions_path(conn, :index)
+  end
+
   def index(conn, _params) do
     conn
     |> put_layout(false)
--- test/web/controllers/page_controller_test.exs
+++ test/web/controllers/page_controller_test.exs
@@ -1,6 +1,14 @@
 defmodule Podcatcher.Web.PageControllerTest do
   use Podcatcher.Web.ConnCase
 
+  test "GET / if user logs in redirects to their feed", %{conn: conn} do
+
+    conn = conn
+      |> assign(:user, %Podcatcher.Accounts.User{})
+      |> get("/")
+    assert redirected_to(conn) =~ "/feed"
+  end
+
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Get started"
